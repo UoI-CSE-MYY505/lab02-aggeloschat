5bba963814b1960e9d41bdd483a01c23ed5b1a3e
aggeloschat <aggeloschatz02@gmail.com>
2024-10-20 22:16:04 +0300

    Probably Done

--- lab02.s
+++ lab02.s
@@ -12,6 +12,21 @@ prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
+
+findBackwards:
+    beq a1,zero,ret0
+    slli s0,a1,2
+    add s0,s0,a0
+loop:
+    addi s0,s0,-4
+    lw t1, 0x0(s0)
+    beq t1,a2,done
+    beq s0,a0,ret0
+    j loop
+
+ret0:
+    add s0,zero,zero # Not found
+
 #-----------------------------
 done:
     addi a7, zero, 10 
